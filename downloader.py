9fc1296e9d993f807e42ee96b23a17637eb2f914
jasper <jasper.alani@colart.com>
2020-06-12 04:21:40 +0100

    Adds simple_downloader.py

--- downloader.py
+++ downloader.py
@@ -1,8 +1,33 @@
+""" 
+Downloader requires requests and tqdm
+$ pip install requests tqdm urllib
+
+https://www.occultboards.com/ebooks/satanism/777.pdf
+"""
+
+import urllib
 import requests
 from tqdm import tqdm
+from urllib2 import Request, urlopen, URLError
 
-def download_file ():
-    file_url = input("Enter file file_url: ")
+def get_file_url ():
+    url = raw_input('Enter file url: ')
+    req = Request(url)
+    try:
+        response = urlopen(req)
+    except URLError, e:
+        if hasattr(e, 'reason'):
+            print 'We failed to reach a server.'
+            print 'Reason: ', e.reason
+            return False
+        elif hasattr(e, 'code'):
+            print 'The server couldn\'t fulfill the request.'
+            print 'Error code: ', e.code
+            return False
+    else:
+        return url
+
+def download_file (file_url):
     file_name = input("Enter file name: ")
     # Streaming, so we can iterate over the response.
     r = requests.get(file_url, stream=True)
@@ -18,4 +43,12 @@ def download_file ():
     if total_size != 0 and t.n != total_size:
         print("ERROR, something went wrong")
 
-download_file()
+file_url = get_file_url()
+
+if( file_url ): # If successful
+    download_file( file_url ) # Download file
+else:
+    print("Failed to get file url")
+
+
+# download_file()
---
+++ file_url.py
@@ -0,0 +1,15 @@
+from urllib2 import Request, urlopen, URLError
+
+url = raw_input('enter something')
+req = Request(url)
+try:
+    response = urlopen(req)
+except URLError, e:
+    if hasattr(e, 'reason'):
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+    elif hasattr(e, 'code'):
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+else:
+    print 'URL is good!'
